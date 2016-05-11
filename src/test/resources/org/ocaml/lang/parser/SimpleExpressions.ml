ae5114a6aa7d29ce54fea77b4304c3088f45336f
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-05-11 19:42:56 +0530

    Added empty begin end expression to parser

--- src/test/resources/org/ocaml/lang/parser/SimpleExpressions.ml
+++ src/test/resources/org/ocaml/lang/parser/SimpleExpressions.ml
@@ -9,4 +9,5 @@ begin [@ identifier 123] "abc" end;
 begin % when "abc" end;
 begin [@ identifier : ;; ] "abc" end;
 (*begin [@ identifier : 'identifier ] "abc" end;*)
-begin [@ identifier : val a : 'identifier [@@ identifier 123 ]] "abc" end
+begin [@ identifier : val a : 'identifier [@@ identifier 123 ]] "abc" end;
+begin end
