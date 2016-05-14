fb6aa261f5839e00486b22cffe293d0e4461d3ec
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-05-14 19:03:59 +0530

    Added a bunch of expr rules

--- src/test/resources/org/ocaml/lang/parser/SimpleExpressions.ml
+++ src/test/resources/org/ocaml/lang/parser/SimpleExpressions.ml
@@ -33,4 +33,34 @@ abc def ~ghi ?klm ?opq:rst ~uvw:xyz;
 (while true do 123 done);
 (for i = 1 to 10 do 123 done);
 (::)(1, 3);
-1 :: 2 :: 3
+(1 :: 2 :: 3);
+(1 = 2 < 3 > 4);
+(1 < 4 :: 2 = 3);
+(1 +  2 * 3 / 4 := 5 || 7 & 8 @ 9 ** 2);
+(1 + -2);
+(a.b <- c)
+(a.("abc"; "123") <- b);
+(a.["abc"; "123"] <- b)
+(a.{b} <- c);
+(a <- b);
+object
+    (*val mutable virtual x : int
+    val virtual mutable y : int
+    val mutable z = "hello"
+    val w : int = 123
+    method private virtual m : 'a 'b . int
+    inherit Mod1.Mod2.klass as klassy
+    inherit fun a b c -> Mod1.Mod2.klass as klassy
+    inherit klass a b
+    inherit [klass1, klass2] a b
+    inherit (klass a b : identifier)
+    inherit ! [@identifier abc] [@identifier 123] [klass1, klass2] a b [@identifier 123]*)
+    inherit (klass a b : object
+        (*[@identifier abc] (int)
+        val virtual mutable a : int
+        constraint int = string*)
+        [%% abc : val a: int]
+    end) [@identifier abc]
+end
+
+
