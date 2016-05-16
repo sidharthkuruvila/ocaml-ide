ba3d5e71600a67d5e928a13713dd83121855f753
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-05-16 18:53:54 +0530

    Added more bits to the let constructs in the parser

---
+++ src/main/kotlin/org/ocaml/lang/parser/OcamlParserUtil.kt
@@ -0,0 +1,10 @@
+package org.ocaml.lang.parser
+
+import com.intellij.lang.parser.GeneratedParserUtilBase
+
+/**
+ * Created by sidharthkuruvila on 16/05/16.
+ */
+object OcamlParserUtil : GeneratedParserUtilBase() {
+
+}
