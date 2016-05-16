7c4639be41031d53e721298545ee6cf6978571b6
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-05-16 23:14:55 +0530

    Added commenter

--- src/main/kotlin/org/ocaml/ide/commenter/OcamlCommenter.kt
+++ src/main/kotlin/org/ocaml/ide/commenter/OcamlCommenter.kt
@@ -1,5 +1,20 @@
 package org.ocaml.ide.commenter
 
+import com.intellij.lang.Commenter
+
 /**
  * Created by sidharthkuruvila on 16/05/16.
  */
+
+class OcamlCommenter : Commenter {
+    override fun getCommentedBlockCommentPrefix(): String? = "(*"
+
+    override fun getCommentedBlockCommentSuffix(): String? = "*)"
+
+    override fun getBlockCommentPrefix(): String? = "(*"
+
+    override fun getBlockCommentSuffix(): String? = "*)"
+
+    override fun getLineCommentPrefix(): String? = null
+
+}
