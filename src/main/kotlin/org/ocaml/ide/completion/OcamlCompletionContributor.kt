202489d6e12a897964b858afbf29a38d307ebe40
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-05-17 11:02:37 +0530

    Added dummy completion contributor

---
+++ src/main/kotlin/org/ocaml/ide/completion/OcamlCompletionContributor.kt
@@ -0,0 +1,23 @@
+package org.ocaml.ide.completion
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.util.ProcessingContext
+import org.ocaml.lang.lexer.OcamlTypes
+
+/**
+ * Created by sidharthkuruvila on 04/05/16.
+ */
+class OcamlCompletionContributor : CompletionContributor() {
+    init {
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement(),
+                object : CompletionProvider<CompletionParameters>() {
+                    override fun addCompletions(parameters: CompletionParameters,
+                                       context: ProcessingContext,
+                                       resultSet: CompletionResultSet) {
+                        resultSet.addElement(LookupElementBuilder.create("Hello"));
+                    }
+                })
+    }
+}
