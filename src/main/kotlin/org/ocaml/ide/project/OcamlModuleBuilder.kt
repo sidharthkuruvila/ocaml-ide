8cb186fadc3834c705f57e51c8e73d94e91e0b13
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-05-23 14:19:31 +0530

    Added the module directory as the source route

--- src/main/kotlin/org/ocaml/ide/project/OcamlModuleBuilder.kt
+++ src/main/kotlin/org/ocaml/ide/project/OcamlModuleBuilder.kt
@@ -3,6 +3,8 @@ package org.ocaml.ide.project
 import com.intellij.ide.util.projectWizard.ModuleBuilder
 import com.intellij.openapi.module.ModuleType
 import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.LocalFileSystem
 
 /**
  * Created by sidharthkuruvila on 18/05/16.
@@ -13,6 +15,9 @@ class OcamlModuleBuilder : ModuleBuilder() {
     }
 
     override fun setupRootModel(modifiableRootModel: ModifiableRootModel?) {
-        println("hello")
+        val path = contentEntryPath!!
+        val sourceRoot = LocalFileSystem.getInstance().refreshAndFindFileByPath(FileUtil.toSystemIndependentName(path))!!;
+        val ce = doAddContentEntry(modifiableRootModel)!!
+        ce.addSourceFolder(sourceRoot, false)
     }
 }
