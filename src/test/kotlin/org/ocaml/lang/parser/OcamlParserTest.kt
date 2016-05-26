8bdbdf3257660aebfce833b6b4fa346f0d1cb783
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-05-26 23:37:09 +0530

    Removed test case SimpleExpression2

--- src/test/kotlin/org/ocaml/lang/parser/OcamlParserTest.kt
+++ src/test/kotlin/org/ocaml/lang/parser/OcamlParserTest.kt
@@ -1,9 +1,6 @@
 package org.ocaml.lang.parser
 
-import com.intellij.openapi.util.io.FileUtil
 import com.intellij.testFramework.ParsingTestCase
-import org.ocaml.merlin.BrowseNode
-import org.ocaml.merlin.Merlin
 import org.ocaml.test.OcamlC
 import java.io.File
 
@@ -23,19 +20,6 @@ class OcamlParserTest : ParsingTestCase("org/ocaml/lang/parser", "ml", OcamlPars
 
 
     }
-
-    fun testSimpleExpressions2() {
-        try {
-            OcamlC.assertParses(getTestFile())
-        } catch (e: java.lang.AssertionError) {
-            e.printStackTrace()
-        }
-        doTest(true)
-        OcamlC.assertParses(getTestFile())
-
-
-    }
-
 
     override fun getTestDataPath(): String {
         return "src/test/resources";
