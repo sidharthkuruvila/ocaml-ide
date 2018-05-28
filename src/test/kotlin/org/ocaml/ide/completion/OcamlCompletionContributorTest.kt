0d1226a2d1c2447298bde34a2dddf3f97d2dd35b
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2018-05-28 16:25:30 +0530

    Added some sanity tests to check if the languages were configured corrrectly.

--- src/test/kotlin/org/ocaml/ide/completion/OcamlCompletionContributorTest.kt
+++ src/test/kotlin/org/ocaml/ide/completion/OcamlCompletionContributorTest.kt
@@ -16,6 +16,8 @@ class OcamlCompletionContributorTest : LightPlatformCodeInsightFixtureTestCase()
         let a = TestModule.some_<caret>"""
         val expected =  arrayOf("TestModule.some_other_val", "TestModule.some_val")
         myFixture.configureByText("test.ml", script)
+
+
         val res = this.myFixture.complete(CompletionType.BASIC).map { it.lookupString }.toTypedArray()
         UsefulTestCase.assertOrderedEquals(expected, *res)
     }
