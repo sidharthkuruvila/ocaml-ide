db7312d618b580ef0a8a4f54f27ff833c6146b94
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2018-05-28 13:26:10 +0530

    Added some logging to OcamlCompletionContributor

--- src/main/kotlin/org/ocaml/ide/completion/OcamlCompletionContributor.kt
+++ src/main/kotlin/org/ocaml/ide/completion/OcamlCompletionContributor.kt
@@ -3,6 +3,7 @@ package org.ocaml.ide.completion
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.util.ProcessingContext
 import org.ocaml.ide.components.MerlinServiceComponent
@@ -11,18 +12,24 @@ import org.ocaml.util.ReversedSubstringCharSequence
 
 class OcamlCompletionContributor : CompletionContributor() {
 
+    companion object {
+        private val LOG = Logger.getInstance(this::class.java)
+    }
+
     val merlinService = ApplicationManager.getApplication().getComponent(MerlinServiceComponent::class.java)
 
     init {
-
+        LOG.debug("Enabling OcamlCompletionContributor backed by merlin.")
         extend(CompletionType.BASIC, PlatformPatterns.psiElement(),
                 object : CompletionProvider<CompletionParameters>() {
                     override fun addCompletions(parameters: CompletionParameters,
                                        context: ProcessingContext,
                                        resultSet: CompletionResultSet) {
                         val ln = LineNumbering(parameters.originalFile.text)
+                        LOG.debug("About to fetch completions")
                         val completions = merlinService.completions(parameters.originalFile,
                                 findSuitablePrefix(parameters), ln.position(parameters.offset))
+                        LOG.debug("Fetch completions $completions")
                         for(completion in completions) {
                             resultSet.addElement(LookupElementBuilder.create(completion.name).withTypeText(completion.desc));
                         }
